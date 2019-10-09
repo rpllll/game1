5fcb09eb13afc9b59d8bdb33db4570cfc15c9df2
Travis <travis.lambert@yahoo.com>
2019-10-09 17:21:38 -0500

    setup

---
+++ 2DGame/Assets/boatMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boatMovement : MonoBehaviour {
+
+	public float Speed = 10f;
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float Dirx = Speed * Time.deltaTime;
+		transform.position = new Vector2 (transform.position.x + Dirx, transform.position.y);
+
+	}
+
+}
