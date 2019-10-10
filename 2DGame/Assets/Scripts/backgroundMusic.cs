8da19cbd350230907b3abfd9c3a6d44054f90cd1
rpl17 <56279086+rpl17@users.noreply.github.com>
2019-10-10 00:13:30 -0500

    music and menu

---
+++ 2DGame/Assets/Scripts/backgroundMusic.cs
@@ -0,0 +1,24 @@
+﻿ using UnityEngine;
+ using System.Collections;
+ using UnityEngine.SceneManagement;
+ 
+ public class backgroundMusic : MonoBehaviour {
+ 
+     void Awake ()
+     {
+         GameObject[] objs = GameObject.FindGameObjectsWithTag("music");
+         if (objs.Length > 1)
+             Destroy(this.gameObject);
+ 
+         DontDestroyOnLoad(this.gameObject);
+ 
+     }
+ 
+     void Update()
+     {
+         if (SceneManager.GetActiveScene().name == "SceneName")
+         {
+             Destroy(this.gameObject);
+         }
+     }
+ }
